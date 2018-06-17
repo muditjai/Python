091c7f08d4868c45fae6aa1a0be19d8af1820391
Mudit Jain <muditjai@gmail.com>
2018-06-17 15:03:12 -0700

    update to v2.3

--- Distribution/setup.py
+++ Distribution/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='VedicCityNames',
-      version='0.2.2',
+      version='0.2.3',
       description='Translates current city names in India to their original names',
       long_description='Long description - Translates current city names in India to their original names',
       url='http://github.com/muditjai/Python.git/Distribution/VedicCityNames',
